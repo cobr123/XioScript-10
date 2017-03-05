2c3ee0e2cdeb662a2b77877761b674e37fbe4df6
cobr123 <cobr123@users.noreply.github.com>
2017-03-05 13:18:15 +0300

    Update XSUS.user.js

--- XSUS.user.js
+++ XSUS.user.js
@@ -2,17 +2,17 @@
 // @name           XioScript
 // @namespace      Virtonomics
 // @description    XioScript using XioLibraries
-// @version        10.1.5
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
-// @require        http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCF.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSML.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCL.js
-// @require        https://raw.githubusercontent.com/XiozZe/XioScript/master/XSEL.js
-// @resource       jQuiCss     https://raw.githubusercontent.com/XiozZe/XioScript/master/jquery-ui.css
-// @resource       myCss       https://raw.githubusercontent.com/XiozZe/XioScript/master/XSCSS.css
-// @include        http://*virtonomic*.*/*/*
-// @exclude        http://virtonomics.wikia.com*
+// @version        10.1.6
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
+// @require        https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
+// @require        https://raw.githubusercontent.com/cobr123/XioScript-10/master/XSCF.js
+// @require        https://raw.githubusercontent.com/cobr123/XioScript-10/master/XSML.js
+// @require        https://raw.githubusercontent.com/cobr123/XioScript-10/master/XSCL.js
+// @require        https://raw.githubusercontent.com/cobr123/XioScript-10/master/XSEL.js
+// @resource       jQuiCss     https://raw.githubusercontent.com/cobr123/XioScript-10/master/jquery-ui.css
+// @resource       myCss       https://raw.githubusercontent.com/cobr123/XioScript-10/master/XSCSS.css
+// @include        http*://*virtonomic*.*/*/*
+// @exclude        http*://virtonomics.wikia.com*
 // @grant          GM_getResourceText
 // @grant          GM_addStyle
 // ==/UserScript==
